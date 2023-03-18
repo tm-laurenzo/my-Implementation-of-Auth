f1c967cc6d1ed3b142ba6d81246dd2fcad8ee6bc
tm-laurenzo <60081627+tm-laurenzo@users.noreply.github.com>
2023-03-18 16:33:39 +0100

    made changes to the server

--- api/server.js
+++ api/server.js
@@ -9,16 +9,16 @@ import jwt from 'jsonwebtoken';
 import Todo from './models/Todo.js';
 
 const secret = 'secret123';
-await mongoose.connect('mongodb://localhost:27017/auth-todo', 
-          {useNewUrlParser:true, useUnifiedTopology:true});
+await mongoose.connect('mongodb://localhost:27017/auth-todo',
+  { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.log);
 
 const app = express();
 app.use(cookieParser());
-app.use(bodyParser.json({extended:true}));
+app.use(bodyParser.json({ extended: true }));
 app.use(cors({
-  credentials:true,
+  credentials: true,
   origin: "http://localhost:3000",
 }));
 
@@ -36,41 +36,41 @@ app.get('/user', (req, res) => {
       if (!userInfo) {
         return res.json({});
       }
-      res.json({id:userInfo._id,email:userInfo.email});
+      res.json({ id: userInfo._id, email: userInfo.email });
     });
 });
 
 app.post('/register', (req, res) => {
-  const {email, password} = req.body;
-  const hashedpassword = bcrypt.hashSync(password,10);
-  const user = new User({password: hashedpassword, email: email});
+  const { email, password } = req.body;
+  const hashedpassword = bcrypt.hashSync(password, 10);
+  const user = new User({ password: hashedpassword, email: email });
   user.save().then(userInfo => {
-    jwt.sign({id:userInfo._id,email:userInfo.email}, secret, (err,token) => {
+    jwt.sign({ id: userInfo._id, email: userInfo.email }, secret, (err, token) => {
       if (err) {
         console.log(err);
         res.sendStatus(500);
       } else {
-        res.cookie('token', token).json({id:userInfo._id,email:userInfo.email});
+        res.cookie('token', token).json({ id: userInfo._id, email: userInfo.email });
       }
     });
   });
 });
 
 app.post('/login', (req, res) => {
-  const {email,password} = req.body;
-  User.findOne({email})
+  const { email, password } = req.body;
+  User.findOne({ email })
     .then(userInfo => {
       if (!userInfo) {
         return res.sendStatus(401);
       }
       const passOk = bcrypt.compareSync(password, userInfo.password);
       if (passOk) {
-        jwt.sign({id:userInfo._id,email},secret, (err,token) => {
+        jwt.sign({ id: userInfo._id, email }, secret, (err, token) => {
           if (err) {
             console.log(err);
             res.sendStatus(500);
           } else {
-            res.cookie('token', token).json({id:userInfo._id,email:userInfo.email});
+            res.cookie('token', token).json({ id: userInfo._id, email: userInfo.email });
           }
         });
       } else {
@@ -83,24 +83,24 @@ app.post('/logout', (req, res) => {
   res.cookie('token', '').send();
 });
 
-app.get('/todos', (req,res) => {
+app.get('/todos', (req, res) => {
   const payload = jwt.verify(req.cookies.token, secret);
-  Todo.where({user:new mongoose.Types.ObjectId(payload.id)})
-    .find((err,todos) => {
+  Todo.where({ user: new mongoose.Types.ObjectId(payload.id) })
+    .find((err, todos) => {
       res.json(todos);
     })
 });
 
-app.put('/todos', (req,res) => {
+app.put('/todos', (req, res) => {
   const payload = jwt.verify(req.cookies.token, secret);
   const todo = new Todo({
-    text:req.body.text,
-    done:false,
-    user:new mongoose.Types.ObjectId(payload.id),
+    text: req.body.text,
+    done: false,
+    user: new mongoose.Types.ObjectId(payload.id),
   });
   todo.save().then(todo => {
     res.json(todo);
   })
 });
 
-app.listen( 4000);
+app.listen(4000);
